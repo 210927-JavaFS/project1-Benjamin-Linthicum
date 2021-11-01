74de0e1c31cf9d5244989e0d66c6ab8e5590d841
Benjamin-Linthicum <benlinth@udel.edu>
2021-10-31 22:26:17 -0400

    Some front end work, setting up login and registration

--- src/main/resources/static/Project1.js
+++ src/main/resources/static/Project1.js
@@ -1,13 +1,21 @@
 const URL = "http://localhost:8081/";
 
 let loginButton = document.getElementById('loginButton');
+let toRegistrationButton = document.getElementById('toRegistration');
+let toLoginButton = document.getElementById('toLogin');
+
+let registerElements = Array.prototype.slice.call(document.getElementsByClassName('registerClass'));
+let loginElements = Array.prototype.slice.call(document.getElementsByClassName('loginClass'));
+registerElements.forEach(e => e.style.display = "none");
 
 loginButton.onclick = login;
+toLoginButton.onclick = navigateToLogin;
+toRegistrationButton.onclick = navigateToRegistration;
 
 async function login(){
     let user = {
-        username:document.getElementById("username").value,
-        password:document.getElementById("password").value
+        username:document.getElementById("loginUsername").value,
+        password:document.getElementById("loginPassword").value
     }
 
     let response = await fetch(URL+"login", {
@@ -26,7 +34,17 @@ async function login(){
             message.id = "loginFailed";
             message.setAttribute("style", "color:red");
             message.innerText = "LOGIN FAILED";
-            document.getElementsByClassName("loginClass").appendChild(message);
+            document.getElementsByClassName("loginClass")[0].appendChild(message);
         }
     }
+}
+
+function navigateToRegistration(){
+    registerElements.forEach(e => e.style.display = "inline");
+    loginElements.forEach(e => e.style.display = "none");
+}
+
+function navigateToLogin(){
+    registerElements.forEach(e => e.style.display = "none");
+    loginElements.forEach(e => e.style.display = "inline");
 }
